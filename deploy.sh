77ad274867b3475eb36d2cbfc0e2983bfc908722
shankar <jaisiva151990@gmail.com>
2018-07-26 09:11:43 -0700

    file modified

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ if [$ENVIRONMENT = "QA" ]; then
   for i in 'cat ip.txt'
     do
       echo "Deploy war file $i:"
-      sshpass -p "1234" scp target/ola.war siva@172.17.0.3:/home/siva/soft/apache-tomcat-8.5.32/webapps
+      sshpass -p "1234" scp target/ola.war siva@$i:/home/siva/soft/apache-tomcat-8.5.32/webapps
       sleep 3
       echo "Start the $i tomcat server"
-      sshpass -p "1234" ssh siva@172.17.0.3 "JAVA_HOME=/home/siva/soft/jdk1.8.0_171" "/home/siva/soft/apache-tomcat-8.5.32/bin/./startup.sh"
+      sshpass -p "1234" ssh siva@$i "JAVA_HOME=/home/siva/soft/jdk1.8.0_171" "/home/siva/soft/apache-tomcat-8.5.32/bin/./startup.sh"
     done
   echo "Deploy success"
 fi
