76ca99cec0c6f971b204edd15c8103abacbc7011
shankar <jaisiva151990@gmail.com>
2018-07-26 07:08:42 -0700

    Deploy war file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
-for i in `cat ip.txt`
-do
-echo "depoy war file"
+#!/bin/bash
+ENVIRONMENT = $1
+if [ENVIRONMENT = "QA" ]; then
+  for i in 'cat ip.txt'
+    do
+      echo "Deploy war file $i:"
+      sshpass -p "1234" scp target/ola.war siva@172.17.0.3:/home/siva/soft/apache-tomcat-8.5.32/webapps
+      echo "Start the $i tomcat server"
+      sshpass -p "1234" ssh siva@172.17.0.3 "JAVA_HOME=/home/siva/soft/jdk1.8.0_171" "/home/siva/soft/apache-tomcat-8.5.32/bin/./startup.sh"
+    done
+  echo "Deploy success"
+fi
